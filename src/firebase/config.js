eef07fe2c883bee7ae75c08b85588ff506b5e927
Adem ilter <ademilter@gmail.com>
2018-02-14 22:23:56 +0300

    proje update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,6 @@
 export default {
-  apiKey: 'AIzaSyD7wCsK2NiIDG-Ct4DLhUq_U9dgKISwW5w',
-  authDomain: 'feyzli-linkler.firebaseapp.com',
-  databaseURL: 'https://feyzli-linkler.firebaseio.com',
-  projectId: 'feyzli-linkler',
-  storageBucket: 'feyzli-linkler.appspot.com',
-  messagingSenderId: '96266793180'
+  apiKey: 'AIzaSyAnbjVK_WzSfjPujLST_iPNmGce0jsbKSs',
+  authDomain: 'profil-cards.firebaseapp.com',
+  databaseURL: 'https://profil-cards.firebaseio.com',
+  projectId: 'profil-cards'
 }
