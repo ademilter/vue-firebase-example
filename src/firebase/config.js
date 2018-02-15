97c4394f4eba855716c55fc07a5e5d1890f9bde5
Adem ilter <ademilter@gmail.com>
2018-02-15 11:30:17 +0300

    bilgileri gizlemenin anlamı yokmuş :*

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
 export default {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: ''
+  apiKey: 'AIzaSyBbLqy_mg2dGCooHiVtPNLOrz4hmPxRJxk',
+  authDomain: 'profile-cards.firebaseapp.com',
+  databaseURL: 'https://profile-cards.firebaseio.com',
+  projectId: 'profile-cards'
 }
--- src/firebase/config.local.js
+++
@@ -1,6 +0,0 @@
-export default {
-  apiKey: 'AIzaSyBbLqy_mg2dGCooHiVtPNLOrz4hmPxRJxk',
-  authDomain: 'profile-cards.firebaseapp.com',
-  databaseURL: 'https://profile-cards.firebaseio.com',
-  projectId: 'profile-cards'
-}
